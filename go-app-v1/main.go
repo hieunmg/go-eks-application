0d74ac84e1c006261913b50b0a9116bd623fa90c
hieunmg <hieungming@gmail.com>
2025-01-30 22:17:48 +0700

    ngrok

--- go-app-v1/main.go
+++ go-app-v1/main.go
@@ -1,95 +1,37 @@
+// A simple HTTP service.
+
 package main
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"log/slog"
+	"log"
 	"net/http"
-	"strings"
-)
 
-const (
-	host = "0.0.0.0"
-	port = 8080
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
 )
 
 func main() {
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-
-	defer cancel()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/app-v1", func(w http.ResponseWriter, r *http.Request) {
-		JSONResponse(w, http.StatusOK, map[string]interface{}{
-			"message": "app-v1",
-		})
-	})
-
-	s := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", host, port),
-		Handler: allowCORS(withLogger(mux)),
-	}
-
-	go func() {
-		<-ctx.Done()
-		slog.Info("shutting down the http server")
-
-		if err := s.Shutdown(context.Background()); err != nil {
-			slog.Error("failed to shutdown http server", err)
-		}
-	}()
-
-	slog.Info("start listening...", "address", fmt.Sprintf("%s:%d", host, port))
-
-	if err := s.ListenAndServe(); errors.Is(err, http.ErrServerClosed) {
-		slog.Error("failed to listen and serve", err)
+	if err := run(context.Background()); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func allowCORS(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
-				preflightHandler(w, r)
-
-				return
-			}
-		}
-		h.ServeHTTP(w, r)
-	})
-}
-
-func preflightHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	headers := []string{"*"}
-	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
-
-	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
-	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
-
-	slog.Info("preflight request", "http_path", r.URL.Path)
-}
-
-func withLogger(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("Run request", "http_method", r.Method, "http_url", r.URL)
-
-		h.ServeHTTP(w, r)
-	})
-}
-
-func JSONResponse(w http.ResponseWriter, status int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	// Marshal the data into JSON and write it to the response
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func run(ctx context.Context) error {
+	ln, err := ngrok.Listen(ctx,
+		config.HTTPEndpoint(),
+		ngrok.WithAuthtokenFromEnv(),
+	)
+	if err != nil {
+		return err
 	}
+
+	log.Println("Ingress established at:", ln.URL())
+
+	return http.Serve(ln, http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from ngrok-go!")
 }
